cb769646896d07757326f12e9e9c6956e7b8de6e
auto login <autologin@ib-410-07.ipa.psati.ru>
2023-05-13 17:17:04 +0300

    Установка ззначения поля стипендии

--- student.py
+++ student.py
@@ -1,8 +1,16 @@
 class Student:
-    def __init__(self, name, age, id):
+    def __init__(self, name, age, id,):
         self.name = name
         self.age = age
         self.id = id
+        self.salary = None
 
     def change_name(self, new_name):
         self.name = new_name
+    def set_salary(self, salary):
+        self.salary = salary
+        
+
+        
+
+    
