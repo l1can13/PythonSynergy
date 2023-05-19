200e894e1bf56cce379703c5ca1d50515d059b24
l1can13 <a.a.soroka05@gmail.com>
2023-05-19 18:43:47 +0400

    calculator class

--- calculator.py
+++ calculator.py
@@ -1,27 +1,2 @@
-#Умножать два числа(a-первый множитель,b-второй множитель)
-def multiplication(a,b):
-    return a*b
-
-#функция делит 2 числа (x и y)
-def division(x, y):
-    return x / y
-    
-# Складывает два числа(a-первое слагаемое, b-второе слагаемое)
-def plus(a, b):
-    plus=a+b
-    return plus
-
-# вычитание lde вычитание двух чисел(а- уменьшаемое b-вычетаемое )
-def mainus(a,b):
-    mainus=a-b
-    return mainus
-
-#функция извлекает квадратный корень 
-def extraction(x):
-    y=x**0.5
-    return y
-
-# Возведение в степень n(a-число, b-степень)
-def stepn(a, b):
-    stepn=a**b
-    return stepn
+class Calculator:
+    pass
--- main.py
+++ main.py
@@ -1,28 +1,2 @@
-from student import Student
+from calculator import Calculator
 
-name = 'Вася Пупкин'
-age = 16
-id = 1
-fakultet = 'медицинский'
-
-
-student1 = Student(name, age, id)
-
-print(student1.fakultet)
-
-student1.change_fakultet('медицинский')
-
-print(student1.fakultet)
-print(student1.get_avg_mark())
-print(student1.nam
-
-student1.get_birth_year()
-
-print(student1.name)
-
-student1.difference()
-
-print(student1.difference())
-print(student1.get_birth_year())
-student1.get_num_letters_name()
-print(student1.get_num_letters_name())
