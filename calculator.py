b0fb7c21da12e45126d83defb33592e0761de818
auto login <autologin@ib-410-04.ipa.psati.ru>
2023-05-20 16:11:45 +0300

    инвертирование и 2 в n-ной степени

--- calculator.py
+++ calculator.py
@@ -1,2 +1,24 @@
 class Calculator:
-    pass
+    @ staticmethod
+    def add(a,b):
+        """складывает два значения
+        params:
+         а - первое слагаемое.
+         b - второе слагаемое.
+        return: результат сложение a,b.
+        """
+        return a+b
+    def add(n):
+        """ значеие 2 в степени n
+        params:
+        n = n-ная степеть
+        return: результат 2 в n-ной степени
+        """
+        return 2^n
+    def add(у):
+        """инвертирование
+        params:
+         а - число 
+        return: инвертировали число в минус n-ной степени
+        """
+        return 1/у^-1
