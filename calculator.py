b9872997bec5a75e80d20805855f5185adf061d5
auto login <autologin@ib-410-01.ipa.psati.ru>
2023-05-20 21:34:51 +0800

    на проверку

--- calculator.py
+++ calculator.py
@@ -1,2 +1,41 @@
+import math
+
 class Calculator:
-    pass
+    @staticmethod
+    def add(a,b):
+        """
+        складывай два числа.
+
+        params:
+             a-первое слогаемое
+             b-второе слогаемое
+        1`return - результат сложения a;b.
+        """
+        return a+b
+
+    @staticmethod
+    def grad_to_rad(grad):
+        return grad / 360 * math.pi * 2
+
+    @staticmethod
+    def cos(a):
+        """
+        находит косинус заданного угла
+
+        params:
+            a-угол
+            return- значение косинуса
+        """
+        return math.cos(Calculator.grad_to_rad(a))
+    @staticmethod
+    def sin(a):
+        """
+        находит синус заданного угла
+
+        params:
+            a-угол
+            return- значение синуса
+        """
+        return math.sin(Calculator.grad_to_rad(a))
+
+        
