26b5f9654c7f784117ed19dbe91d145c0b189a1a
auto login <autologin@ib-410-07.ipa.psati.ru>
2023-05-20 16:18:20 +0300

    Вычитание,Умножение

--- calculator.py
+++ calculator.py
@@ -1,2 +1,39 @@
 class Calculator:
-    pass
+    @staticmethod
+    def add(A,B):
+        """
+        Складывает два значения.
+
+        params:
+            A-Первое слагаемое.
+            B-Второе слагаемое.
+
+        return: Результат сложения A,B.
+        """
+        return A+B
+
+    @staticmethod
+    def sub(A,B):
+            """
+            Вычитается два числа.
+
+            params:
+                A-Уменьшаемое.
+                B-Вычитаемое.
+
+            return: Результат вычитаемого A,B.
+            """
+            return A-B
+
+            @staticmethod
+    def multiplication(A,B):
+        """
+        Умножается два числа.
+
+        params:
+             A-Множимое.
+             B-Множитель.
+
+        return: Результат умножения A,B.
+                """
+        return A*B
