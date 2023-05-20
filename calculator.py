6b5ba2f74620707b3654f49d98b82e2731d2f53f
auto login <autologin@ib-410-06.ipa.psati.ru>
2023-05-20 21:21:09 +0800

    zadanie n3

--- calculator.py
+++ calculator.py
@@ -1,2 +1,32 @@
 class Calculator:
-    pass
+    """def add(a, b):
+        slozhenie.
+
+        params: 
+            a - pervoe ch.
+            b - vtoroe ch.
+        return: result slozhenia a, b.
+        
+       return a+b
+       """
+    @ staticmethod
+    def root_extraction(a, n,):
+        """
+        vivod korna n-stepeni
+        params:
+            a - ch
+            n - stepen korna
+            returne: n koren iz a
+        """
+        return a**(1/n)
+
+    @ staticmethod
+    def exponentiation(a, n,):
+        """
+        vvod v stepen
+        params:
+            a - ch
+            n - stepen 
+            returne: a v stepeni n
+        """
+        return a**n
