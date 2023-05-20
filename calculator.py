820f7a72095c782838f9c1cc74e5fe120c5015d9
auto login <autologin@ib-410-03.ipa.psati.ru>
2023-05-20 17:02:44 +0400

    Деление, деление по модулю.

--- calculator.py
+++ calculator.py
@@ -1,2 +1,48 @@
 class Calculator:
-    pass
+    @staticmethod
+    def add(a, b):
+        """
+           Складывает два значения.
+
+           params:
+                  a-первое слагаемое
+                  b-второе слагаемое
+
+           return: результат сложения a, b
+        """
+        return a+b
+
+
+
+
+
+    @staticmethod
+    def add(a, b):
+        """
+           Деление двух значений.
+
+           params:
+                  a-первое слагаемое
+                  b-второе слагаемое
+
+           return: результат деления a, b
+        """
+        return a/b
+
+
+
+
+
+    
+    @staticmethod
+    def add(a, b):
+        """
+           Деление двух значений по модулю.
+
+           params:
+                  a-первое слагаемое
+                  b-второе слагаемое
+
+           return: результат деления по модулю a, b
+        """
+        return a%b
