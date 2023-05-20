81e37bd4aea1c89cd70f28d043cc1712eae38ec4
auto login <autologin@ib-410-08.ipa.psati.ru>
2023-05-20 16:13:34 +0300

    1-Выведение модуля 2-Нахождение факторила числа b

--- calculator.py
+++ calculator.py
@@ -1,2 +1,38 @@
+from math import factorial
 class Calculator:
-    pass
+    @staticmethod
+    def add(a,b):
+        """
+            Складывает 2 значения.
+
+            parans:
+            a-1-ое слогаемое.
+            b-2-ое слогаемое.
+
+            return:результат сложения a и b.
+       """
+        return a+b
+
+    def abs(a):
+        """
+            Делает модуль из числа.
+
+            parans:
+            a-число которое хотим перевести в модуль.
+
+            return:число в модуле.
+        """
+        return abs(a)
+
+    def factorial(b):
+        """
+            Находит факториал числа.
+
+            parans:
+            b-число факториал которого хотим найти.
+
+            return:факориал числа.
+        """
+        return factorial(b)
+       
+       
