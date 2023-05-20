b2d913c39bf24ecd378a86b7c5e75dba4ce0ca6d
auto login <autologin@ib-410-05.ipa.psati.ru>
2023-05-20 21:23:58 +0800

    сообщение

--- calculator.py
+++ calculator.py
@@ -1,2 +1,28 @@
+from math import tanh,ctg
 class Calculator:
-    pass
+    @staticmethod
+    def add(a,b):
+        """Складывает два значения.
+        params:
+            a - первое.
+            b - второое.
+        return: результат ложения a,b.
+        """
+        return a+b
+
+    def tg(a):
+        """Вычисляет тангенс a.
+        params:
+        a - первое.
+        tanh - вычисляет тангенс.
+        return: результат вычисления.
+        """
+        return tanh(a)
+    def ctg(a):
+        """Вычисляет котангенс a.
+        params :
+        a - первое.
+        ctg - вычесляет котангенс.
+        return: результат вычисления 
+        """
+        return ctg(a)
